d0abca3f92b64bfa172ba91e7ce7ef4fb160b398
Carlos Spagnolo <carlos.andres.spagnolo@gmail.com>
2024-04-02 12:51:37 -0300

    Mejora del SEO y se agrega constantes de idioma

---
+++ src/utils/constants.ts
@@ -0,0 +1,72 @@
+export const CONTENT = {
+  es: {
+    navigation: {
+      home: "Inicio",
+      about: "Sobre mi",
+      projects: "Proyectos",
+      contact: "Contacto",
+      stack: "Herramientas",
+    },
+    greeting: "Hola! 👋 Soy",
+    profession: "Desarrollador Web Frontend",
+    about:
+      "Soy un desarrollador web frontend con un gran enfoque en la elaboración de experiencias web dinámicas e interactivas. Mi conjunto de herramientas incluye React y Next.js para páginas dinámicas que no solo son visualmente atractivas, sino también de alto rendimiento. Además, utilizo Astro para crear páginas web estáticas ultrarrápidas que ofrecen una experiencia de usuario fluida.",
+    projects: {
+      title: "Algunos Proyectos",
+      view: "Ver en GitHub",
+      description: {
+        all: "Este es el primer intento de crear un sitio de comercio electrónico.",
+        spagnolo:
+          "Sitio de comercio electrónico. Cuenta con una página de inicio, catálogo, descripción del producto, inicio de sesión y carrito de compras próximamente.",
+        sunnyside:
+          "Este proyecto es una solución al reto de landing page de la agencia Sunnyside en Frontend Mentor.",
+        coffee:
+          "Este proyecto es la solución al reto de devChallerges llamado Simple Coffee.",
+        blog: "Blog desarrollado con Next 13 y Tailwind, que utiliza su propia api.",
+        translate:
+          "Este proyecto es la solución al reto de devChallerges llamado Translate App.",
+      },
+    },
+    form: {
+      name: "Tu nombre",
+      email: "Tu email",
+      text: "Tu mensaje",
+      send: "Enviar",
+    },
+  },
+  en: {
+    navigation: {
+      home: "Home",
+      about: "About",
+      projects: "Projects",
+      contact: "Contact",
+      stack: "Stack",
+    },
+    greeting: "Hi! 👋 I'm",
+    profession: "Frontend Developer",
+    about:
+      "I am a passionate frontend developer with a keen focus on crafting dynamic and interactive web experiences. My toolkit includes React and Next.js for dynamic pages that are not only visually appealing but also high-performing. Additionally, I leverage Astro to create lightning-fast, static web pages that offer a seamless user experience.",
+    projects: {
+      title: "Some Projects",
+      view: "View on GitHub",
+      description: {
+        all: "This is the first attempt to create an e-commerce site.",
+        spagnolo:
+          "E-commerce site. It has a home page, catalog, product description, login and shopping cart coming soon.",
+        sunnyside:
+          "This project is a solution to the Sunnyside agency landing page challenge on Frontend Mentor.",
+        coffee:
+          "This project is the solution to the devChallerges challenge called Simple Coffee.",
+        blog: "Blog site developed with Next 13 and Tailwind, which uses its own api.",
+        translate:
+          "This is the solution to the devChallerges challenge called Translate App.",
+      },
+    },
+    form: {
+      name: "Your name",
+      email: "Your email",
+      text: "Your message",
+      send: "Send",
+    },
+  },
+};
